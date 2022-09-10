01f058c30c2c002de8ea88ed3057141365c43054
Suppenhuhn79 <ch.zager@web.de>
2022-09-10 21:29:39 +0200

    typo

--- js/lib/tools.d.js
+++ js/lib/tools.d.js
@@ -1,7 +1,7 @@
 /**
  * @callback ChoiceCallback
  * @param {String} choiceKey Key of chosen item
- * @param {Boolean} interactive `true` if the choice was set by a (user) event, otherwise `false`
+ * @param {Boolean} interactive `true` if the choice was set by an (user) event, otherwise `false`
  *
  * @callback vikbCallback
  * @param {"opened"|"closed"} event `"opened"` if virtual keyboard just popped on, or `"closed"` if it popped off
